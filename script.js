2a663ae141264d64040d766d1bfd162602e1058e
Prommin Inkanha <promminkew@gmail.com>
2026-02-24 01:32:43 +0700

    update WebToDoList

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 function showTextfill() {
-    let inputText = document.getElementById("inputText").value;
+    let input = document.getElementById("inputText");
+    let text = input.value;
+
+    if (text.trim() === "") return;
+
+    createTask(text);
+
+    input.value = "";
+}
+
+function createTask(text) {
+
+    let taskContainer = document.createElement("div");
+
     let displayText = document.createElement("p");
-    displayText.innerText = inputText;
-    let results = document.getElementById("displayText");
-    results.appendChild(displayText);
-    document.getElementById("inputText").value = "";
+    displayText.innerText = text;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "ลบ";
+
+    deleteBtn.addEventListener("click", function () {
+        deletetask(taskContainer);
+    });
+
+    taskContainer.appendChild(displayText);
+    taskContainer.appendChild(deleteBtn);
+
+    document.getElementById("displayText").appendChild(taskContainer);
+}
+
+function deletetask(taskElement) {
+    taskElement.remove();
 }
